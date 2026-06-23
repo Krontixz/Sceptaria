696ecc25afd2121c9aa0ca068c3b479e525c80f9
GibberFish <zakariyah6204@mytusd.org>
2026-06-23 11:19:24 -0700

    added play button

--- net/sceptaria/sceptaria/home.js
+++ net/sceptaria/sceptaria/home.js
@@ -3,3 +3,7 @@ const playButton = document.getElementById('play-button');
 playButton.addEventListener('click', function() {
     alert("Thank's For Playing Our Game! It Will Be Starting Shortly");
 });
+// Play Button Href Event
+playButton.addEventListener('click', function() {
+    window.Location.href = "./game/game.html";
+});
